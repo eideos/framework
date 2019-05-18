b10f9031a304a81a5d01b5ba70a13d37ca092133
Federico Bouzas <federicobouzas@gmail.com>
2019-05-18 18:32:47 -0300

    Daterange

--- public/js/presentation/daterange.js
+++ public/js/presentation/daterange.js
@@ -1,17 +1,13 @@
 function daterange_init(field, params) {
-  $('[name="tmp_' + field.attr("name") + '_from"]').datepicker({
-    uiLibrary: 'bootstrap4',
-    locale: 'es-es',
-    format: 'dd/mm/yyyy'
+  var name = field.attr("name");
+  $("[name='" + name + "[from]']").datepicker({
+      uiLibrary: 'bootstrap4',
+      locale: 'es-es',
+      format: 'dd/mm/yyyy'
   });
-  $('[name="tmp_' + field.attr("name") + '_to"]').datepicker({
-    uiLibrary: 'bootstrap4',
-    locale: 'es-es',
-    format: 'dd/mm/yyyy'
+  $("[name='" + name + "[to]']").datepicker({
+      uiLibrary: 'bootstrap4',
+      locale: 'es-es',
+      format: 'dd/mm/yyyy'
   });
-  $('[name="tmp_' + field.attr("name") + '_from"],[name="tmp_' + field.attr("name") + '_to"]').change(function() {
-    var from = $('[name="tmp_' + field.attr("name") + '_from"]').val();
-    var to = $('[name="tmp_' + field.attr("name") + '_to"]').val();
-    field.val(from + "|" + to);
-  }).change();
 }
---
+++ src/Presentations/pst_daterange.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Presentations;
+
+use Eideos\Framework\Lib\Presentation;
+
+class pst_daterange extends Presentation {
+
+    protected $view = "framework::presentations.daterange";
+    public $js_initial = "daterange_init";
+
+    public function getJsIncludes() {
+        return ["vendor/framework/js/daterange.js"];
+    }
+
+    public function getViewVars() {
+        return array_merge(parent::getViewVars(), [
+            "value_from" => $this->value["from"],
+            "value_to" => $this->value["to"],
+        ]);
+    }
+
+    public function getHelperValue() {
+        $value = $this->getValue();
+        $parts = [];
+        if (!empty($this->value["from"])) {
+            $parts[] = "Desde: " . $this->value["from"];
+        }
+        if (!empty($this->value["to"])) {
+            $parts[] = "Hasta: " . $this->value["to"];
+        }
+        return implode(" | ", $parts);
+    }
+
+    public function loadDatabaseValue() {
+        if (isset($this->value)) {
+            return implode(",", $this->value);
+        }
+        return null;
+    }
+
+    public function getDatabaseWhere($queryField = null) {
+        $queryField = $queryField ?? $this->getOriginalName();
+        $parts = [];
+        if (!empty($this->value["from"])) {
+            $parts[] = $queryField . " >= '" . self::transformValue($this->value["from"]) . " 00:00:00'";
+        }
+        if (!empty($this->value["to"])) {
+            $parts[] = $queryField . " <= '" . self::transformValue($this->value["to"]) . " 23:59:59'";
+        }
+        return implode(" AND ", $parts);
+    }
+
+    public static function transformValue($value) {
+        if (empty($value)) {
+            return null;
+        }
+        $array = explode("/", $value);
+        return implode("-", array_reverse($array));
+    }
+
+}
