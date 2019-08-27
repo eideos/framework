f7a875cbd50f50a86895256d62a80507a661a364
Martin <martincrespop@gmail.com>
2019-08-27 11:54:25 -0300

    fixes pst_time

--- src/Presentations/pst_time.php
+++ src/Presentations/pst_time.php
@@ -4,25 +4,36 @@ namespace Eideos\Framework\Presentations;
 
 use Eideos\Framework\Lib\Presentation;
 
-class pst_time extends Presentation {
+class pst_time extends Presentation
+{
 
     protected $view = "framework::presentations.time";
     public $js_initial = "time_init";
-
-    public function getSearchBarValue($search = "") {
+    public $format = "H:i";
+    public function getSearchBarValue($search = "")
+    {
         return "LIKE '%" . self::transformValue($search) . "%'";
     }
 
-    public function getInitialValue() {
+    public function getInitialValue()
+    {
         $params = $this->getJsParams();
         if (empty($this->initialvalue) && isset($params['force']) && $params['force']) {
-            return date("H:i");
+            return date($this->format);
         }
         return $this->initialvalue;
     }
 
-    public function getJsIncludes() {
-        return ["vendor/framework/js/presentation/time.js"];
+    public function loadDatabaseValue()
+    {
+        if (!empty($this->value)) {
+            return date($this->format, strtotime($this->value));
+        }
+        return "";
     }
 
+    public function getJsIncludes()
+    {
+        return ["vendor/framework/js/presentation/time.js"];
+    }
 }
