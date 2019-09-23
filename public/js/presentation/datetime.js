d461a433887f423f7813bf0b081c6a276201338f
Martin <martincrespop@gmail.com>
2019-09-23 09:37:50 -0300

    Fix locale datetime

--- public/js/presentation/datetime.js
+++ public/js/presentation/datetime.js
@@ -2,6 +2,7 @@ function datetime_init(field, params) {
   if (field.siblings("[readonly]").length == 0) {
     field.datetimepicker({
       uiLibrary: 'bootstrap4',
+      locale: 'es-es',
       format: params.format,
       modal: true,
       footer: true
