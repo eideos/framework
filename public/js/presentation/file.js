2d978997989c64f376c1345b384f8a54e6a89ab6
Martín Crespo <martincrespop@gmail.com>
2019-11-04 14:07:38 -0300

    fix

--- public/js/presentation/file.js
+++ public/js/presentation/file.js
@@ -14,3 +14,24 @@ function file_init(field, params) {
     }
   });
 }
+
+function file_topopup(model, number , field) {
+  var html_preview = $("[id='table["+model+"]["+number+"]["+field+"]Preview']").html();
+  $("[name='tablepopup[" + model + "][" + field + "]']").parent().find('.file-preview-thumbnails').html(html_preview);
+  presentation_topopup(model, number, field);
+}
+
+function file_tovalue(model, field) {
+  var number = tableEditNumber;
+  if (number == null){
+    number = tablesNextNumber[model] || 1;
+  }
+  var input_file_name = "table{" + model + "}{" + number + "}{" + field.name + "}";
+  var clone_input_file = $("[name='tablepopup[" + model + "][" + field.name + "]_file']").clone();
+  $("[name='" + input_file_name + "_file']").remove();
+  clone_input_file.attr('name', input_file_name + "_file");
+  clone_input_file.attr('id', input_file_name + "File");
+  clone_input_file.attr('style', 'display:none');
+  $("table[data-model='" + model+ "']").append(clone_input_file);
+  return $("[name='tablepopup[" + model + "][" + field.name + "]']").val();
+}
