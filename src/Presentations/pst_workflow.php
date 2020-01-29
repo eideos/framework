f431fbedcb70fc1a3ab21e3865e5edd9a4ff65bf
Federico Bouzas <federicobouzas@gmail.com>
2020-01-28 23:42:46 -0300

    FIX

--- src/Presentations/pst_workflow.php
+++ src/Presentations/pst_workflow.php
@@ -14,8 +14,8 @@ class pst_workflow extends Presentation
 
     public function __construct($params) {
         parent::__construct($params);
-        if(!empty($params)){
-            $dParams = json_decode($this->params, true);
+        if (!empty($this->params)){
+            $dParams = json_decode(str_replace("'", '"', $this->params), true);
             if (isset($dParams['searchfield']) && $dParams['searchfield']) {
                 $this->searchfield = true;
             }
