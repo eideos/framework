a94685006aef24066efa7ecebe4d15c444846b01
Martín Crespo <martincrespop@gmail.com>
2020-03-22 13:07:06 -0300

    fix

--- public/js/presentation/date.js
+++ public/js/presentation/date.js
@@ -1,9 +1,20 @@
 function date_init(field, params) {
-    if (field.siblings("[readonly]").length == 0) {
-      field.datepicker({
-          uiLibrary: 'bootstrap4',
-          locale: 'es-es',
-          format: 'dd/mm/yyyy'
-      });
+  if (field.siblings("[readonly]").length == 0) {
+    var paramsdp = {
+        uiLibrary: 'bootstrap4',
+        locale: 'es-es',
+        format: 'dd/mm/yyyy'
+    };
+    if(params.datepicker){
+      paramsdp = params.datepicker;
+      if(paramsdp.minDate){
+        console.log(paramsdp.minDate);
+        paramsdp.minDate = eval(paramsdp.minDate);
+      }
+      if(paramsdp.maxDate){
+        paramsdp.maxDate = eval(paramsdp.maxDate);
+      }
     }
+    field.datepicker(paramsdp);
+  }
 }
