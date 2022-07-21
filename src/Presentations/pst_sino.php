a1360337e7e9e33a91039c48d0e4e16255f6613b
Martin Crespo <martincrespop@gmail.com>
2022-07-21 16:10:25 -0300

    Fixes

--- src/Presentations/pst_sino.php
+++ src/Presentations/pst_sino.php
@@ -2,8 +2,15 @@
 
 namespace Eideos\Framework\Presentations;
 
-class pst_sino extends pst_selectarray {
-
+class pst_sino extends pst_selectarray
+{
     protected $values = [1 => "Si", 0 => "No"];
 
+    public function getDatabaseValue()
+    {
+        if (isset($this->value) && $this->value != '') {
+            return is_array($this->value) ? implode(",", $this->value) : $this->value;
+        }
+        return null;
+    }
 }
