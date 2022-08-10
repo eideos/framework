a6ad5ce8636fda4d0336256eefa0d6238571057c
Martin Crespo <martincrespop@gmail.com>
2022-08-10 08:23:15 -0300

    Agrego variables de bloqueo para administrar desde .env FMW_MINUTES_HEARTBEAT_BLOCK (Minutos en los que agranda y achica el boton de bloqueo) FMW_MINUTES_CHECK_BLOCK (Minutos en el que muestra el cartel de alerta con tiempo restante) FMW_MINUTES_MAX_BLOCK (Minuto maximo de bloqueo)

--- src/views/sections/heartbeat.blade.php
+++ src/views/sections/heartbeat.blade.php
@@ -1,11 +1,13 @@
 @include('framework::sections.jsvars')
 <script type="text/javascript">
-$(function () {
-    var timeChequeo = 5 * 60 * 1000; // 5 minutos
-    var timeLatido = 1 * 60 * 1000; // 1 minuto
-    setInterval("bloqueoRegistroActualizar(" + timeChequeo + ");", timeChequeo);
-    setInterval("bloqueoHearthbeat();", timeLatido);
-});
+    var FMW_MINUTES_CHECK_BLOCK = "{{env('FMW_MINUTES_CHECK_BLOCK', 5)}}";
+    var FMW_MINUTES_HEARTBEAT_BLOCK = "{{env('FMW_MINUTES_HEARTBEAT_BLOCK', 1)}}";
+    $(function() {
+        var timeChequeo = FMW_MINUTES_CHECK_BLOCK * 60 * 1000; // 5 minutos
+        var timeLatido = FMW_MINUTES_HEARTBEAT_BLOCK * 60 * 1000; // 1 minuto
+        setInterval("bloqueoRegistroActualizar(" + timeChequeo + ");", timeChequeo);
+        setInterval("bloqueoHearthbeat();", timeLatido);
+    });
 </script>
 <div class="heartbeat bg-warning" data-toggle="tooltip" data-placement="bottom" title="Otros usuarios no podrán utilizar este registro mientras Ud. lo esté viendo.">
     <i class="fa fa-lock" aria-hidden="true"></i>
