3ecfb696ebe8af80f9f5be5f66b240bd63985961
Martin Crespo <martincrespop@gmail.com>
2022-11-04 09:09:29 -0300

    Fix

--- src/Audit.php
+++ src/Audit.php
@@ -4,10 +4,21 @@ namespace Eideos\Framework;
 
 use App\AppModel;
 
-class Audit extends AppModel {
+class Audit extends AppModel
+{
 
-    public function user() {
+    protected $fillable = [
+        'created_by',
+        'updated_by',
+        'user_type',
+        'user_id',
+        'auditable_type',
+        'auditable_id',
+        'event'
+    ];
+
+    public function user()
+    {
         return $this->belongsTo('Eideos\Framework\User', 'user_id');
     }
-
 }
